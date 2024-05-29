e6c1b0958129d1c1c4f6aa45a207eb0f4aac2416
alee <alee@chou.im>
2024-05-30 00:35:47 +0800

    update example

--- examples/wifi-sta/main.js
+++ examples/wifi-sta/main.js
@@ -1,14 +1,22 @@
 import * as wifi from "wifi"
 
 async function main(){
-
-    if( ! await wifi.connect("your SSID", "your PASSWORD") ){
+    
+    // 连接到 wifi 热点
+    if( ! await wifi.connect("alee's home", "43215678") ){
         console.log("Failed to connect to AP")
         return
     }
+
     console.log("Connected to AP")
 
-
+    // 等待 DHCP 分配 IP
+    let status = await wifi.waitIP()
+    if( !status ){
+        console.log("got ip faild")
+    } else {
+        console.log("STA IP:", status.ip)
+    }
 }
 
 main()
